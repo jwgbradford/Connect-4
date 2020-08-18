f0d3d22a2cab36a1faaf6c076d31c99c575bea69
John Bradford <john.bradford@digilocal.org.uk>
2020-08-18 17:17:40 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+Basic game of Connect 4 in a row
+
+Uses pygame and matrix lists to hold game state
+Uses functions for the game actions
+
+Two players - Red and Yellow
+Players take it in turns to drop a counter in one of 7 columns
+Columns are selected via the number keys 1-7
+
+After each counter is dropped, the game checks to see if there
+are 4-in-a-row, checks are made down, right, down-right, and up-right
+the checks are then reversed to check the other directions
+
+The winner is printed to terminal
+
+A new game can be started at any time by pressing K_SPACE
---
+++ connect4.py
@@ -0,0 +1,64 @@
+import pygame
+
+pygame.init()
+window = pygame.display.set_mode((720,620))
+pygame.display.set_caption('Connect4')
+
+BLACK, BLUE, RED, YELLOW = (0, 0, 0), (20, 50, 200), (200, 0, 0), (200, 200, 50)
+
+def make_piece(colour):
+    image = pygame.Surface((80, 80))
+    image.fill(BLUE)
+    pygame.draw.circle(image, colour, (40, 40), 40)
+    return image
+
+def new_game():
+    game_pieces = [[0 for i in range(6)] for j in range(7)]
+    return game_pieces
+
+def insert_piece(column, turn):
+    for a in range(5, -1, -1):
+        if game_pieces[column][a] == 0:
+            game_pieces[column][a] = turn
+            check_for_win(column, a, turn)
+            turn = 3 - turn
+            return turn
+
+def check_direction(column, row, dx, dy, turn):
+    add_length = 0
+    x, y  = column + dx, row + dy
+    while 0 <= x <= 6 and 0 <= y <= 5:
+        if game_pieces[x][y] == turn:
+            add_length += 1
+            x += dx
+            y += dy
+        else:
+            break
+    return add_length
+
+def check_for_win(column, row, turn):
+    for dir in [[1, 0], [0, 1], [1, 1], [1, -1]]:
+        length = 1
+        length += check_direction(column, row, dir[0], dir[1], turn)
+        length += check_direction(column, row, -dir[0], -dir[1], turn)
+        if length >3:
+            print('Player', turn, 'is the winner!')
+            return
+
+game_pieces = new_game()
+coloured_circles = make_piece(BLACK), make_piece(RED), make_piece(YELLOW)
+turn = 1
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if 49 <= event.key <= 55:
+                turn = insert_piece(event.key - 49, turn)
+            if event.key == pygame.K_SPACE:
+                game_pieces = new_game()
+    window.fill(BLUE)
+    for a in range(0, 7):
+        for b in range(0, 6):
+            team = game_pieces[a][b]
+            window.blit(coloured_circles[team], (20+100*a, 20+100*b))
+    pygame.display.update()
